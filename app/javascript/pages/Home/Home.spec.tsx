8f72f739eb20c28498384b2f81bafb89fbbff4f4
Etienne <e.morel1991@gmail.com>
2023-06-15 13:17:19 +0200

    feat: finish the home page

---
+++ app/javascript/pages/Home/Home.spec.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from ".";
+// needed because of the use of <Link /> on the page
+import { MemoryRouter } from "react-router-dom";
+
+it("renders the home page", () => {
+  const component = render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>
+  );
+
+  expect(component).toMatchSnapshot();
+});
+
+it("links to the list of recipes", () => {
+  const component = render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByRole("button").getAttribute("href")).toBe("/recipes");
+});
--- app/javascript/pages/Home/index.tsx
+++ app/javascript/pages/Home/index.tsx
@@ -3,7 +3,14 @@ import { Link } from "react-router-dom";
 
 export default () => (
   <main className="flex flex-col items-center justify-center h-screen">
-    <h1 className="p-4 text-3xl font-bold">Welcome to Scaling Waffle</h1>
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-center">
+        Welcome to the Scaling Waffle
+      </h1>
+      <h2 className="mt-4 text-1xl text-center">
+        The recipes that scale with your hunger
+      </h2>
+    </div>
     <Link
       to="/recipes"
       className="btn btn-lg"
